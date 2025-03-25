86005c91717caeb6f2a5d8b5eb2f4c501935f3ba
Mikeyhendrix <msaint-vil@albany.edu>
2025-03-24 20:13:41 -0400

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,14 @@
+//using let
+let name = "Michael";
+let age = "21";
+let isStudent = "True";
+
+document.getElementById("demo1").innerHTML =
+"<br>Name: " + name + "<br>Age: " + age + "<br>Student: " + isStudent;
+
+let favColor = "Red";
+let favFood = "steak";
+let favSport = "football";
+
+document.getElementById("demo2").innerHTML =
+"<br>Favorite Color: " + favColor + "<br>Favorite Food: " + favFood + "<br>Favorite Sport: " +favSport;
